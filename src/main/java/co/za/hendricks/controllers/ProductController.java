cb6a4e3e82b75d595830a1aaa6496252edb41350
aziz <aziz@aziz-Precision-M4600>
2014-01-29 20:17:17 +0200

    added @RequestBody to test json body input

--- src/main/java/co/za/hendricks/controllers/ProductController.java
+++ src/main/java/co/za/hendricks/controllers/ProductController.java
@@ -6,12 +6,14 @@
 
 package co.za.hendricks.controllers;
 
-import co.za.hendricks.dao.ProductDAO;
+import co.za.hendricks.domain.Book;
+import co.za.hendricks.domain.Game;
 import co.za.hendricks.domain.Product;
 import co.za.hendricks.services.ProductService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,14 +27,20 @@ public class ProductController {
     @Autowired
     private ProductService productService;
     
-    @RequestMapping("/createProduct")
-    public @ResponseBody Product createProduct(
-           @RequestParam(value="title", required=false, defaultValue="default title")String title){
-        Product testProduct = new Product();
-        testProduct.setTitle(title);
-        productService.createProduct(testProduct);
+    @RequestMapping("/createBook")
+    public @ResponseBody Book createBook(
+           @RequestBody Book book){
+        productService.createProduct(book);
+        return book;
         
-        return testProduct;
+    }    
+    
+    @RequestMapping("/createGame")
+    public @ResponseBody Product createGame(
+           @RequestParam(value="game", required=true)Game game){
+
+        productService.createProduct(game);        
+        return game;
     }
     
     @RequestMapping("/listProducts")
